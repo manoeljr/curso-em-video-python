f8d8e30a3fd97a4cedd7e6682f18f355709f05ac
Manoeljr <manoelv8@gmail.com>
2022-04-01 20:49:02 -0300

    Adicionando mais exercicios em python fundamentos

--- fundamentos/exercicios/ex029.py
+++ fundamentos/exercicios/ex029.py
@@ -3,3 +3,10 @@
     mostre uma mensagem dizendo que ele foi multado. A Multa vai custar R$7.00 por cada
     Km acima do limite.
 """
+velocidade = int(input('Digite a velocidade do veiculo : '))
+
+if velocidade > 80:
+    print(f'Velocidade acima de 80Km/h a multa é R${(velocidade - 80) * 7}')
+
+print(f'{velocidade}Km/h velocidade permitida.')
+
