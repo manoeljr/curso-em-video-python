1f70320ce375d6806171522c17708e328b725156
Manoeljr <manoelv8@gmail.com>
2022-04-01 21:32:25 -0300

    Adicionando mais exercicios em python fundamentos

--- fundamentos/exercicios/ex033.py
+++ fundamentos/exercicios/ex033.py
@@ -2,3 +2,12 @@
     Faça um programa que leia três números e mostre qual é o MAIOR
     e qual é o MENOR
 """
+primeiro_valor = int(input('Digite o primeiro valor : '))
+segungo_valor = int(input('Digite o segundo valor : '))
+terceiro_valor = int(input('Digite o terceiro valor : '))
+
+numeros = [primeiro_valor, segungo_valor, terceiro_valor]
+
+print(f'O Maior valor foi {max(numeros)} e o Menor foi {min(numeros)}')
+
+
