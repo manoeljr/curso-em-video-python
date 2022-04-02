8f2ef0cdbff637154284af3d3591756087d9eb41
Manoeljr <manoelv8@gmail.com>
2022-04-01 21:43:01 -0300

    Adicionando mais exercicios em python fundamentos

--- fundamentos/exercicios/ex034.py
+++ fundamentos/exercicios/ex034.py
@@ -3,3 +3,9 @@
     Para salários superiores a R$1.250,00, Calcule um aumento de 10%. Para os inferiores ou iguais
     o aumento é de 15%.
 """
+salario = float(input('Informe o valor do salário : '))
+
+if salario > 1250:
+    print(f'Seu salário teve um aumento de 10%. Agora é R${round(salario + (salario * 0.10), 3)}')
+else:
+    print(f'Seu salário teve um aumento de 15%. Agora é R${round(salario + (salario * 0.15), 3)}')
