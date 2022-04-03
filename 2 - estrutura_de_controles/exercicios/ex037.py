4f5ca46580da0e6928ac469a44a10e6f6ce3dcb0
Manoeljr <manoelv8@gmail.com>
2022-04-02 21:08:25 -0300

    Adicionando mais exercicios em python estrutura de controles

--- 2 - estrutura_de_controles/exercicios/ex037.py
+++ 2 - estrutura_de_controles/exercicios/ex037.py
@@ -16,13 +16,13 @@ conversao_base = int(input('Digite a opção de conversão :'))
 
 if conversao_base == 1:
     binario = bin(numero)
-    print(f'Valor convertido para binário : {binario}')
+    print(f'Valor convertido para binário : {binario[2:]}')
 elif conversao_base == 2:
     octal = oct(numero)
-    print(f'Valor convertido para Octal : {octal}')
+    print(f'Valor convertido para Octal : {octal[2:]}')
 elif conversao_base == 3:
     hexadecimal = hex(numero)
-    print(f'Valor convertido para hexadecimal : {hexadecimal}')
+    print(f'Valor convertido para hexadecimal : {hexadecimal[2:]}')
 else:
     print('Opção Inválida !')
 
