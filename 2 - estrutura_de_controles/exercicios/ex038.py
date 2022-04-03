7928ab7c28c12b0bbf36a2418f6c5d06b93b1721
Manoeljr <manoelv8@gmail.com>
2022-04-02 21:17:25 -0300

    Adicionando mais exercicios em python estrutura de controles

--- 2 - estrutura_de_controles/exercicios/ex038.py
+++ 2 - estrutura_de_controles/exercicios/ex038.py
@@ -5,3 +5,12 @@
      - O Segundo valor é maior
      - Não existe valor maior, os dois são iguais
 """
+numero_um = int(input('Digite o primeiro número :'))
+numero_dois = int(input('Digite o segundo número : '))
+
+if numero_um > numero_dois:
+    print(f'O Primeiro número é maior')
+elif numero_dois > numero_um:
+    print('O Segundo número é maior')
+else:
+    print('Não existe número maior ou menor. Eles são iguais !')
