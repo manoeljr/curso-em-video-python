f7c351a03684d9e16d8b019d5acf6c70f4f43adb
Manoeljr <manoelv8@gmail.com>
2022-04-02 23:06:50 -0300

    Adicionando mais exercicios em python estrutura de controles

--- 2 - estrutura_de_controles/exercicios/ex043.py
+++ 2 - estrutura_de_controles/exercicios/ex043.py
@@ -7,3 +7,18 @@
      - 30 até 40: Obesidade
      - Acima de 40: Obesidade mórbida
 """
+peso = float(input('Informe o peso Kg: '))
+altura = float(input('Informe a altura (m): '))
+
+imc = peso / (altura ** 2)
+
+if imc < 18.5:
+    print(f'Abaixo do peso. IMC = {round(imc, 1)}')
+elif 18.5 <= imc < 25:
+    print(f'Peso ideal. IMC = {round(imc, 1)}')
+elif 25 <= imc < 30:
+    print(f'Sobrepeso. IMC = {round(imc, 1)}')
+elif 30 <= imc < 40:
+    print(f'Obesidade. IMC = {round(imc, 1)}')
+else:
+    print(f'Obesidade mórbida. IMC = {round(imc, 1)}')
