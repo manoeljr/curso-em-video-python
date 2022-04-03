399aac0aba04dffe2c91aa067f760378b400814e
Manoeljr <manoelv8@gmail.com>
2022-04-02 23:22:04 -0300

    Adicionando mais exercicios em python estrutura de controles

--- 2 - estrutura_de_controles/exercicios/ex044.py
+++ 2 - estrutura_de_controles/exercicios/ex044.py
@@ -6,3 +6,23 @@
      - Em até 2x no cartão: preço normal
      - 3x ou mais no cartão: 20% de juros
 """
+preco = float(input('Qual o valor das compras R$: '))
+
+print('''INFORME A FORMA DE PAGAMENTOS
+[ 1 ] á vista dinheiro/cheque
+[ 2 ] á vista cartão 
+[ 3 ] 2x no cartão
+[ 4 ] 3x ou mais no cartão''')
+opcao_pagamento = int(input('Qual a opção ? '))
+
+if opcao_pagamento == 1:
+    desconto = preco - (preco * 0.10)
+    print(f'10% de desconto no produto á vista dinheiro/cheque, novo valor do produto R${desconto}')
+elif opcao_pagamento == 2:
+    desconto = preco - (preco * 0.05)
+    print(f'5% de desconto no produto á vista no cartão, novo valor do produto R${desconto}')
+elif opcao_pagamento == 3:
+    print(f'Valor em até 2x no cartão. Valor do produto R${preco}')
+else:
+    juro = preco + (preco * 0.20)
+    print(f'20% de acrescimo em compras em 3x ou mais no cartão. Novo valor do produto R${juro}')
