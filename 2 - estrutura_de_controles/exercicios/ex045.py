adf4ad73d9a052b796070de5f29af875c144ac94
Manoeljr <manoelv8@gmail.com>
2022-04-03 21:17:49 -0300

    Adicionando mais exercicios em python estrutura de controles

--- 2 - estrutura_de_controles/exercicios/ex045.py
+++ 2 - estrutura_de_controles/exercicios/ex045.py
@@ -1,17 +1,59 @@
 """
     Crie um programa que faça o computador jogar JOKENPÔ(pedra, papel e tesoura) com você
 """
-from random import randrange
+from random import randint
+from time import sleep
 
-escolha = int(input('Vamos jogar ?\n'
-                    'Escolha sua opção :\n'
-                    '1 - Pedra\n'
-                    '2 - Papel\n'
-                    '3 - Tesoura\n'
-                    'Faça sua escolha : '))
+print('''Vamos jogar ?
+Escolha sua opção :
+    [ 1 ] Pedra
+    [ 2 ] Papel
+    [ 3 ] Tesoura
+''')
 
-escolhas = ['pedra', 'papel', 'tesoura']
+jogador = int(input('Qual a sua opão : '))
 
-computador = randrange(escolhas)
+opcoes = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
 
-print(computador)
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PÔ !!!')
+sleep(1)
+
+print('-=' * 11)
+print(f'Computador jogou {opcoes[computador]}')
+print(f'Jogador jogou {opcoes[jogador]}')
+print('-=' * 11)
+
+if computador == 0:
+    if jogador == 0:
+        print('Empate')
+    elif jogador == 1:
+        print('Jogador vence')
+    elif jogador == 2:
+        print('Computador vence')
+    else:
+        print('Jogada Inválida')
+elif computador == 1:
+    if jogador == 0:
+        print('Computador vence')
+    elif jogador == 1:
+        print('Empate')
+    elif jogador == 2:
+        print('Jogador vence')
+    else:
+        print('Jogada Inválida')
+elif computador == 2:
+    if jogador == 0:
+        print('Jogador vence')
+    elif jogador == 1:
+        print('Computador vence')
+    elif jogador == 2:
+        print('Empate')
+    else:
+        print('Jogada Inválida')
+else:
+    print('Jogada Inválida')
