a6d17c903978dff1718b1d366aa5d39e6ff21e90
Manoeljr <manoelv8@gmail.com>
2022-04-08 20:38:06 -0300

    Adicionando mais exercicios em python estrutura de repetição WHILE

--- 4 - estrutura_de_repeticao_while/exercicios/ex062.py
+++ 4 - estrutura_de_repeticao_while/exercicios/ex062.py
@@ -2,3 +2,25 @@
     Melhore o desafio 061, perguntando para o usuário se ele quer mostrar mais
     alguns termos. O Programa encerra quando ele disser que quer mostrar 0 termos
 """
+print('Gerador de PA')
+print('-=' * 10)
+
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print(f'{termo} -> ', end='')
+        termo += razao
+        cont += 1
+
+    print('PAUSE')
+    mais = int(input('Quantos termos você quer mostrar a mais ?'))
+
+print(f'Progressão finalizada com {total} termos mostrados.')
+
