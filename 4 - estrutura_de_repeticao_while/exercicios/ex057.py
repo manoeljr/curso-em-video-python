cbe9d8fb528395be221ac8b5cc789cd00260848e
Manoeljr <manoelv8@gmail.com>
2022-04-08 18:52:41 -0300

    Adicionando mais exercicios em python estrutura de repetição WHILE

--- 4 - estrutura_de_repeticao_while/exercicios/ex057.py
+++ 4 - estrutura_de_repeticao_while/exercicios/ex057.py
@@ -3,15 +3,25 @@
     os valores 'M' ou 'F'. Caso esteja errado, peça a digitação
     novamente até ter um valor correto.
 """
-sexo = True
+#  Uma forma de digitar a sexo
+# sexo = True
+#
+# while(sexo):
+#     print('Digite o sexo : [M/F}')
+#     sexo = str(input('Qual o sexo : ')).strip().upper()[0]
+#
+#     if sexo in 'Mm' or sexo in 'Ff':
+#         sexo = False
+#         break
+#     else:
+#         print('Sexo errada !')
+#         continue
 
-while(sexo):
-    print('Digite o sexo : [M/F}')
-    sexo = str(input('Qual o sexo : '))
+#  Outra forma
+sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
 
-    if sexo in 'Mm' or sexo in 'Ff':
-        sexo = False
-        break
-    else:
-        print('Sexo errada !')
-        continue
+while sexo not in 'MmFf':
+    sexo = str(input('Dados inválidos. Por favor, Informe seu sexo : ')).strip().upper()[0]
+
+print(f'Sexo {sexo} registrado com sucesso.')
+
