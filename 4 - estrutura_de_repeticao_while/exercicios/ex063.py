2ba672b5bd7eb02946402042c17434228a39fc3e
Manoeljr <manoelv8@gmail.com>
2022-04-08 20:54:47 -0300

    Adicionando mais exercicios em python estrutura de repetição WHILE

--- 4 - estrutura_de_repeticao_while/exercicios/ex063.py
+++ 4 - estrutura_de_repeticao_while/exercicios/ex063.py
@@ -3,3 +3,19 @@
     os N primeiros elementos de um sequência de Fibonacci.
     Ex.: 0->1->1->2->3->5->8
 """
+print('=' * 30)
+print('Seguência de Fibonacci')
+print('=' * 30)
+numero = int(input('Quantos termos você quer mostrar ? '))
+termo_1 = 0
+termo_2 = 1
+print('~' * 30)
+print(f'{termo_1} -> {termo_2}', end='')
+cont = 3
+while cont <= numero:
+    termo_3 = termo_1 + termo_2
+    print(f' -> {termo_3}', end='')
+    termo_1 = termo_2
+    termo_2 = termo_3
+    cont += 1
+print(' -> FIM')
