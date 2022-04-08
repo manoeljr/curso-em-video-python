42435dc571af5e4f343c7e79e82058f8182bcf6b
Manoeljr <manoelv8@gmail.com>
2022-04-08 19:57:40 -0300

    Adicionando mais exercicios em python estrutura de repetição WHILE

--- 4 - estrutura_de_repeticao_while/exercicios/ex060.py
+++ 4 - estrutura_de_repeticao_while/exercicios/ex060.py
@@ -3,7 +3,21 @@
     seu fatorial
     Ex.: 5! = 5*4*3*2*1 = 120
 """
-numero = int(input('Digite o número fatorial : '))
+from math import factorial
 
-for i in range(numero, 0, -1):
-    print(f'{i}! = {i}*{i-1} =')
+numero = int(input('Digite o número fatorial : '))
+fatorial = 1
+
+# Solução usando modulo do Python
+print(f'O Fatorial de {numero} é {factorial(numero)}')
+print('-=' * 16)
+# Outra forma de desenvolver o fatorial
+print(f'Calculando {numero}! = ', end='')
+while numero > 0:
+    print(f'{numero}', end='')
+    print(f' x ' if numero > 1 else ' = ', end='')
+    fatorial *= numero
+    numero -= 1
+
+print(f'{fatorial}')
+
