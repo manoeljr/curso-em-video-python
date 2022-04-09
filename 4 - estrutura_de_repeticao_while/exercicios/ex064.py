1d7ec3b36b7f87072d3224a4f21da0cb0b295934
Manoeljr <manoelv8@gmail.com>
2022-04-08 21:35:22 -0300

    Adicionando mais exercicios em python estrutura de repetição WHILE

--- 4 - estrutura_de_repeticao_while/exercicios/ex064.py
+++ 4 - estrutura_de_repeticao_while/exercicios/ex064.py
@@ -3,3 +3,13 @@
     quando o usário digitar o valor qqq, que é a condição de parada. No final, mostre quantos
     números foram digitados e qual foi a soma entre eles(desconsiderando o flag.
 """
+numero = cont = soma = 0
+
+numero = int(input('Digite um número [999 para parar]: '))
+
+while numero != 999:
+    soma += numero
+    cont += 1
+    numero = int(input('Digite um número [999 para parar]: '))
+
+print(f'Você digitou {cont} números e a soma entre eles foi {soma}.')
--- 4 - estrutura_de_repeticao_while/exercicios/ex065.py
+++ 4 - estrutura_de_repeticao_while/exercicios/ex065.py
@@ -3,3 +3,27 @@
     mostre a média entre todos os valores e qual foi o maior e menor, valores lidos.
     O Programa deve perguntar ao usuário se ele quer ou não continuar a digitar valore.
 """
+
+resposta = 'S'
+media_valores = quantidade = soma = maior = menor = 0
+
+while resposta in 'Ss':
+    numero = int(input('Digite um número: '))
+    resposta = str(input('Quer continuar ? [S/N] ')).upper().strip()
+
+    soma += numero
+    quantidade += 1
+
+    if quantidade == 1:
+        maior = menor = numero
+    else:
+        if numero > maior:
+            maior = numero
+        if numero < menor:
+            menor = numero
+
+media_valores = soma / quantidade
+print(f'Você digitou {quantidade} números e a média foi {media_valores}.')
+print(f'O Maior valor foi {maior} e o menor foi {menor}')
+
+
