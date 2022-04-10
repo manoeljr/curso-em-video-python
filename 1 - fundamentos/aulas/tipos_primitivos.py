caff91496c2fa3097a5d444cda313a92996ede08
Manoeljr <manoelv8@gmail.com>
2022-04-10 13:31:24 -0300

    Alterando diretorio nos exercicios de fundamentos

--- 1 - fundamentos/aulas/tipos_primitivos.py
+++
@@ -1,7 +0,0 @@
-"""
-    Tipos primitivos:
-     - Int, inteiro(reais)
-     - Float, flutuante, Numero com ponto, virgula
-     - Bool, verdadeiro(True) ou falso(False)
-     - Str, String(letras)
-"""
