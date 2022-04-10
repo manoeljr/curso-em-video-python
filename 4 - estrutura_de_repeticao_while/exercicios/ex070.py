f1a0a639b8656226f0a3a8d8b698180e4fea3ee0
Manoeljr <manoelv8@gmail.com>
2022-04-10 17:20:39 -0300

    Adicionando mais exercicios em python estrutura de repetição WHILE

--- 4 - estrutura_de_repeticao_while/exercicios/ex070.py
+++ 4 - estrutura_de_repeticao_while/exercicios/ex070.py
@@ -6,3 +6,38 @@
       B) Quantos produtos custam mais de R$1000.
       C) Qual é o nome do produto mais barato.
 """
+from time import sleep
+
+total_gasto_na_compra = quantidade_produto_mais_de_mil = cont = menor_valor_produto = 0
+nome_produto_mais_barato = ''
+
+while True:
+    nome = str(input('Nome do produto: '))
+    preco = float(input('Preço do produto R$: '))
+    cont += 1
+
+    total_gasto_na_compra += preco
+    if preco > 1000:
+        quantidade_produto_mais_de_mil += 1
+
+    if cont == 1:
+        menor_valor_produto = preco
+        nome_produto_mais_barato = nome
+    else:
+        if preco < menor_valor_produto:
+            menor_valor_produto = preco
+            nome_produto_mais_barato = nome
+
+
+    resposta = ''
+    while resposta not in 'SN':
+        resposta = str(input('Deseja continuar [S/N]: ')).strip().upper()[0]
+
+    if resposta == 'N':
+        print('Programa finalizando...')
+        sleep(2)
+        break
+
+print(f'O Total da compra foi R${round(total_gasto_na_compra, 4)}')
+print(f'{quantidade_produto_mais_de_mil} produtos que custam mais de R$1000')
+print(f'O Produto mais barato custa R${round(menor_valor_produto, 4)} e foi {nome_produto_mais_barato}')
