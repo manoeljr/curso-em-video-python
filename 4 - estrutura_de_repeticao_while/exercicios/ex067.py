cf7dc1d57d6d3abddc0c89f9691da37dc084f7e1
Manoeljr <manoelv8@gmail.com>
2022-04-10 15:29:44 -0300

    Adicionando mais exercicios em python estrutura de repetição WHILE

--- 4 - estrutura_de_repeticao_while/exercicios/ex067.py
+++ 4 - estrutura_de_repeticao_while/exercicios/ex067.py
@@ -3,3 +3,18 @@
     para cada valor digitado pelo usuário. O Programa será interrompido
     quando o número solicitado for negativo.
 """
+print('Vamos cria um tabuada de multiplicação')
+print('=' * 15)
+
+while True:
+    numero = int(input('Digite o número inteiro para cria a tabuada: '))
+
+    if numero <= 0:
+        print('=' * 10)
+        print('Tabuada encerrada, até a próxima !')
+        break
+
+    print('=' * 10)
+    for i in range(1, 11):
+        print(f'{numero} x {i} = {numero * i}')
+    print('=' * 10)
