8aa1d30b8ca65a3f0d906580db6eb274524587cf
Manoeljr <manoelv8@gmail.com>
2022-04-12 22:53:16 -0300

    Adicionando mais exercicios em estruturas compostas TUPLA

--- 5 - estrutura_compostas/exercicios/tuplas/ex077.py
+++ 5 - estrutura_compostas/exercicios/tuplas/ex077.py
@@ -2,3 +2,10 @@
     Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
     Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 """
+palavras = ('gratis', 'problema')
+
+for palavra in palavras:
+    print(f' Na palavra {palavra.upper()} temos ', end='')
+    for letra in palavra:
+        if letra.lower() in 'aeiou':
+            print(letra, end='')
