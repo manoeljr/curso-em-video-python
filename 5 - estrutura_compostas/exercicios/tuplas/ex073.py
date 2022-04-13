4f096ed29cb34789ca8cb1d87d7b31ceb674efda
Manoeljr <manoelv8@gmail.com>
2022-04-12 21:31:06 -0300

    Adicionando mais exercicios em estruturas compostas TUPLA

--- 5 - estrutura_compostas/exercicios/tuplas/ex073.py
+++ 5 - estrutura_compostas/exercicios/tuplas/ex073.py
@@ -6,3 +6,16 @@
      C) Uma lista com os times em ordem alfabética
      D) Em que posição na tabela está o time da chapecoense
 """
+times_campeonato_brasileiro = (
+    'Atlético-MG', 'Flamengo', 'Palmeiras', 'Fortaleza', 'Corinthians', 'Bragantino', 'Fluminense', 'América-MG',
+    'Atlético-GO', 'Santos', 'Ceará-SC', 'Internacional', 'São Paulo', 'Athletico-PR', 'Cuiabá', 'Juventude',
+    'Grêmio', 'Bahia', 'Sport Recife', 'Chapecoense'
+)
+
+
+print(f'Os 5 primeiros colocados do campeonato brasileiro -> {times_campeonato_brasileiro[0:5]}')
+print(f'Os 4 últimos colocados do campeonato brasileiro -> {times_campeonato_brasileiro[-4:]}')
+print(f'Tabela em ordem alfabética do campeonato brasileiro -> {sorted(times_campeonato_brasileiro)}')
+print(f'Chapecoense está na {times_campeonato_brasileiro.index("Chapecoense") + 1} posição do campeonato brasileiro')
+
+
