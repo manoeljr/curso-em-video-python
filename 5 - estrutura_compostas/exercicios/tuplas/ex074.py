e9fbb30f08c4f3051553157c7f71151895052515
Manoeljr <manoelv8@gmail.com>
2022-04-12 21:43:50 -0300

    Adicionando mais exercicios em estruturas compostas TUPLA

--- 5 - estrutura_compostas/exercicios/tuplas/ex074.py
+++ 5 - estrutura_compostas/exercicios/tuplas/ex074.py
@@ -3,3 +3,15 @@
     Depois disso, mostre a listagem de números gerados e também indique o menor
     e o maior valor que estão na tupla.
 """
+from random import randint
+
+numeros = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+
+print('Os valores sorteados foram: ', end='')
+for numero in numeros:
+    print(f'{numero} ', end='')
+
+print(f'\nO maior valor sorteado foi {max(numeros)}')
+print(f'O menor valor sorteado foi {min(numeros)}')
+
+
