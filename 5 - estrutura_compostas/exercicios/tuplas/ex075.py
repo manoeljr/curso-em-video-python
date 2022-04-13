3c262ad63f4bb262fbb7cd4f7e0814760e18ce75
Manoeljr <manoelv8@gmail.com>
2022-04-12 22:24:59 -0300

    Adicionando mais exercicios em estruturas compostas TUPLA

--- 5 - estrutura_compostas/exercicios/tuplas/ex075.py
+++ 5 - estrutura_compostas/exercicios/tuplas/ex075.py
@@ -5,3 +5,21 @@
      B) Em que posição foi digitado o primeiro valor 3
      C) Quais foram os números pares
 """
+valores = []
+numeros_pares = []
+cont_nove = 0
+
+for numero in range(4):
+    valores.append(int(input('Digite um número: ')))
+
+valores = tuple(valores)
+
+for i in valores:
+    if i == 9:
+        cont_nove += 1
+    elif i % 2 == 0:
+        numeros_pares.append(i)
+
+print(f'Quantas vezes apareceu o número 9 -> {cont_nove}')
+print(f'Primeiro número 3 localiza-se na posição -> {valores.index(3) + 1}')
+print(f'Os números pares foram -> {numeros_pares}')
