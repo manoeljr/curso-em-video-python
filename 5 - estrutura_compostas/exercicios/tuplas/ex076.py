ddcb6f29d52e980b6097b58618aee62e677580fa
Manoeljr <manoelv8@gmail.com>
2022-04-12 22:39:35 -0300

    Adicionando mais exercicios em estruturas compostas TUPLA

--- 5 - estrutura_compostas/exercicios/tuplas/ex076.py
+++ 5 - estrutura_compostas/exercicios/tuplas/ex076.py
@@ -3,3 +3,21 @@
     respectivos preços, na sequência. No final, mostre uma listagem de preços,
     organizando os dados em forma tabular.
 """
+listagem = (
+    'macarrão', 2.00,
+    'feijão', 10.00,
+    'arroz', 5.00,
+    'açúcar', 2.50,
+    'sal', 1.00,
+    'farinha', 4.35
+)
+
+print('_' * 40)
+print('Lista de produto e preços ')
+print('_' * 40)
+for produto in range(0, len(listagem)):
+    if produto % 2 == 0:
+        print(f'{listagem[produto]:.<30}', end='')
+    else:
+        print(f'R${listagem[produto]:>7.2f}')
+print('_' * 40)
