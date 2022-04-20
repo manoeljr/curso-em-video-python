17e0242b502eb53e360e3fcebeeae051ba1b3a41
Manoeljr <manoelv8@gmail.com>
2022-04-19 23:14:35 -0300

    Adicionando mais exercicios em estruturas compostas TUPLA

--- 5 - estrutura_compostas/exercicios/listas/ex083.py
+++ 5 - estrutura_compostas/exercicios/listas/ex083.py
@@ -3,3 +3,21 @@
     Seu aplicativo deverá analisar se a expressão passada está com os
     parênteses abertos e fechados na ordem correta.
 """
+expressao = str(input('Digite a expressão: '))
+
+pilha = []
+
+for simbolo in expressao:
+    if simbolo == '(':
+        pilha.append('(')
+    elif simbolo == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+
+if len(pilha) == 0:
+    print('Sua expressão está válida !')
+else:
+    print('Expressão está errada !')
