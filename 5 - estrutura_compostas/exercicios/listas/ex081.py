9653b0b530a1bc556c6562b341140d83b1a991ee
Manoeljr <manoelv8@gmail.com>
2022-04-19 22:45:54 -0300

    Adicionando mais exercicios em estruturas compostas LISTA

--- 5 - estrutura_compostas/exercicios/listas/ex081.py
+++ 5 - estrutura_compostas/exercicios/listas/ex081.py
@@ -5,3 +5,18 @@
      B) A lista de valores, ordenada de forma decrescente
      C) Se o valor 5 foi digitado e está ou não na lista
 """
+
+numeros = []
+
+while True:
+    numero = int(input('Digite um número: '))
+    numeros.append(numero)
+    resposta = str(input('Quer continuar ? [S/N]')).split()[0]
+
+    if resposta in 'Nn':
+        break
+
+print('=' * 30)
+print(f'Você digitou {len(numeros)} elementos.')
+print(f'Lista ordenada de forma decrescente -> {numeros.sort(reverse=True)}')
+print(f'O Valor 5 foi digitado -> {"Sim" if 5 in numeros else "Não"}')
