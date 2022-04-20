0575be0d72100b76438cbd52c4112ef533865628
Manoeljr <manoelv8@gmail.com>
2022-04-19 23:00:46 -0300

    Adicionando mais exercicios em estruturas compostas LISTA

--- 5 - estrutura_compostas/exercicios/listas/ex082.py
+++ 5 - estrutura_compostas/exercicios/listas/ex082.py
@@ -4,3 +4,24 @@
     pares e os valores impares digitados, respectivamente. Ao final, mostre
     o conteúdo das três listas geradas.
 """
+numeros = []
+pares = []
+impares = []
+
+while True:
+    numero = int(input('Digite um número: '))
+    resposta = str(input('Digitar outro número ? [S/N]'))
+    numeros.append(numero)
+
+    if numero % 2 == 0:
+        pares.append(numero)
+
+    elif numero % 2 == 1:
+        impares.append(numero)
+
+    if resposta in 'Nn':
+        break
+
+print(f'Lista dos números digitados -> {numeros}')
+print(f'Lista dos números pares -> {pares}')
+print(f'Lista dos números impares -> {impares}')
