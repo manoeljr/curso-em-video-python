7321ee32b4409aa1fe713029d56f4a81964ed96b
Manoeljr <manoelv8@gmail.com>
2022-04-21 20:21:39 -0300

    Adicionando mais exercicios em estruturas compostas LISTA

--- 5 - estrutura_compostas/exercicios/listas/ex086.py
+++ 5 - estrutura_compostas/exercicios/listas/ex086.py
@@ -3,4 +3,17 @@
     preencha com valores lidos pelo teclado No final, mostre
     a matriz na tela, com a formatação correta.
 """
+matriz = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
 
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        matriz[linha][coluna] = int(input(f'Digite os valores para a matriz [{linha}x{coluna}]: '))
+
+print('=' * 10)
+print(matriz[0])
+print(matriz[1])
+print(matriz[2])
