9122459947a8716d0a23ff8f53b1cc42764a1b1b
Manoeljr <manoelv8@gmail.com>
2022-04-21 21:20:02 -0300

    Adicionando mais exercicios em estruturas compostas LISTA

--- 5 - estrutura_compostas/exercicios/listas/ex087.py
+++ 5 - estrutura_compostas/exercicios/listas/ex087.py
@@ -4,3 +4,29 @@
      B) A Soma dos valores da terceira coluna
      C) O Maior valor da segunda linha.
 """
+matriz = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+total = 0
+soma_valores_linha_3 = 0
+
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        matriz[linha][coluna] = int(input(f'Digite os valores para a matriz [{linha}x{coluna}]: '))
+        if matriz[linha][coluna] % 2 == 0:
+            total += matriz[linha][coluna]
+
+for i in range(0, 3):
+    soma_valores_linha_3 += matriz[2][i]
+
+
+print('=' * 10)
+print(matriz[0])
+print(matriz[1])
+print(matriz[2])
+print('=' * 10)
+print(f'A Soma de todos os valores pares -> {total}')
+print(f'A Soma dos valores da terceira linha da matriz -> {soma_valores_linha_3}')
+print(f'O Maior valor da segunda linha da matriz -> {max(matriz[1])}')
