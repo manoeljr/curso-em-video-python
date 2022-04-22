3cf3f1025642e9dc8359a2bc97cc0bd5e7c4b7ec
Manoeljr <manoelv8@gmail.com>
2022-04-21 21:49:34 -0300

    Adicionando mais exercicios em estruturas compostas LISTA

--- 5 - estrutura_compostas/exercicios/listas/ex088.py
+++ 5 - estrutura_compostas/exercicios/listas/ex088.py
@@ -4,3 +4,37 @@
     6 números entre 1 e 60 para cada jogo, cadastrado tudo em uma lista
     composta.
 """
+
+from random import randint
+from time import sleep
+
+lista = []
+jogos = []
+total = 1
+
+print('-' * 30)
+print('     Joga na Mega Sena     ')
+print('-' * 30)
+
+quantidade_de_jogos = int(input('Quantos jogos você quer que eu sorteie ? '))
+
+while total <= quantidade_de_jogos:
+    cont = 0
+    while True:
+        numero = randint(1, 60)
+        if numero not in lista:
+            lista.append(numero)
+            cont += 1
+        if cont >= 6:
+            break
+
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    total += 1
+
+print('=' * 3, f'Sorteando {quantidade_de_jogos} Jogos ', '=' * 3)
+for i, linha in enumerate(jogos):
+    print(f'Jogo {i + 1}: {linha}')
+    sleep(1)
+print('Fim do sorteio de jogos')
