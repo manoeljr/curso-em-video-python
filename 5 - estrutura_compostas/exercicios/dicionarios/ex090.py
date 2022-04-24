104705d8ebd8d4f5588b5087c2345b66d38505c8
Manoeljr <manoelv8@gmail.com>
2022-04-24 12:22:55 -0300

    Adicionando mais exercicios em estruturas compostas DICIONARIO

---
+++ 5 - estrutura_compostas/exercicios/dicionarios/ex090.py
@@ -0,0 +1,5 @@
+"""
+    Faça um programa que leia nome e média de um aluno,
+    guardando também a situação em um dicionário. No final,
+    mostre o contéudo da estrutura na tela.
+"""
---
+++ 5 - estrutura_compostas/exercicios/dicionarios/ex094.py
@@ -0,0 +1,8 @@
+"""
+    Crie um programa que leia nome, sexo e idade de várias pessoas, guardado os dados de cada
+    pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre:
+     A) Quantas pessoas foram cadastradas
+     B) A Média de idade do grupo
+     C) Uma lista com todas as mulheres
+     D) Uma lista com todas as pessoas com idade acima da média
+"""
