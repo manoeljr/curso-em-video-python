e0555e3afd12509a2bfbc9c4748e7af194f41211
manoeljr <manoelv8@gmail.com>
2022-04-26 20:13:39 -0300

    Adicionando exercicios de FUNÇÃO

---
+++ 6 - funcoes/exercicios/ex098.py
@@ -0,0 +1,9 @@
+"""
+    Faça um programa que tenha uma função chamada contador(),
+    que receba três parâmetros: Inicio, fim e passo e realize
+    a contagem. Seu programa tem que realizar três contagens
+    através da função criado:
+     a) De 1 até 10, de 1 em 1
+     b) De 10 até 0, de 2 em 2
+     c) Uma contagem personalizada.
+"""
