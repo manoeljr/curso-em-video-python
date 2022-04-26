444e1cbbfc828d9da340020e5ac1356e92909573
manoeljr <manoelv8@gmail.com>
2022-04-25 22:11:07 -0300

    Adicionando exercicios DICIONARIO

--- 5 - estrutura_compostas/exercicios/dicionarios/ex095.py
+++ 5 - estrutura_compostas/exercicios/dicionarios/ex095.py
@@ -3,3 +3,28 @@
     incluindo um sistema de visualização de detalhes do aproveitamento
     de cada jogador.
 """
+jogador = {}
+partidas = []
+
+jogador['nome'] = str(input('Nome do jogador: '))
+total = int(input(f'Quantas partidas {jogador["nome"]} jogou ?'))
+
+for contador in range(0, total):
+    partidas.append(int(input(f'        Quantos gols na partida {contador + 1} ?')))
+
+jogador['gols'] = partidas[:]
+jogador['total'] = sum(partidas)
+
+print('-=' * 30)
+print(jogador)
+print('-=' * 30)
+for key, value in jogador.items():
+    print(f'O Campo {key} temo valor {value}')
+
+print('-=' * 30)
+print(f'O Jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
+
+for index, value in enumerate(jogador['gols']):
+    print(f'     => Na partida {index}, fez {value} gols.')
+
+print(f'Foi um total de {jogador["total"]} de gols.')
