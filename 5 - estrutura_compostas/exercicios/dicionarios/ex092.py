4893d79cc465a78e556abc156fbbc18be970ad2a
manoeljr <manoelv8@gmail.com>
2022-04-25 21:08:36 -0300

    Adicionando exercicios DICIONARIO

--- 5 - estrutura_compostas/exercicios/dicionarios/ex092.py
+++ 5 - estrutura_compostas/exercicios/dicionarios/ex092.py
@@ -5,3 +5,19 @@
     com quantos anos a pessoa vai se aposentar.
     OBS.: Considere que com 35 anos de carteira assinada você pode se aposentar.
 """
+from datetime import datetime
+
+dados = {}
+dados['nome'] = str(input('Nome: '))
+nascimento = int(input('Ano de nascimento: '))
+dados['idade'] = datetime.now().year -nascimento
+dados['ctps'] = int(input('Carteira de Trabalho (0 não tem): '))
+
+if dados['ctps'] != 0:
+    dados['contratação'] = int(input('Ano de contratação: '))
+    dados['salário'] = float(input('Salário: R$'))
+    dados['aposentadoria'] = dados['idade'] + ((dados['contratação'] + 35) - datetime.now().year)
+
+print('-=' * 30)
+for key, value in dados.items():
+    print(f'  - {key} tem o valor {value}')
