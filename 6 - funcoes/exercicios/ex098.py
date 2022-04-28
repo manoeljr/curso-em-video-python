0fd45a38f32e89089a8c74e01000551222971329
manoeljr <manoelv8@gmail.com>
2022-04-27 21:33:40 -0300

    Adicionando exercicios de FUNÇÃO

--- 6 - funcoes/exercicios/ex098.py
+++ 6 - funcoes/exercicios/ex098.py
@@ -7,3 +7,45 @@
      b) De 10 até 0, de 2 em 2
      c) Uma contagem personalizada.
 """
+from time import sleep
+
+
+def linha_com_20():
+    print('-=' * 20)
+
+
+def contador(inicio, fim, passo):
+
+    if passo < 0:
+        passo *= 1
+    if passo == 0:
+        passo = 1
+
+    linha_com_20()
+    print(f'Contagem de {inicio} até {fim} de {passo} em {passo}')
+
+    if inicio < fim:
+        cont = inicio
+        while cont <= fim:
+            print(f'{cont} ', end='', flush=True)
+            sleep(2.5)
+            cont += passo
+        print('FIM !')
+    else:
+        cont = inicio
+        while cont >= fim:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.5)
+            cont -= passo
+        print('FIM !')
+
+
+contador(1, 10, 1)
+contador(10, 0, 2)
+linha_com_20()
+print('Agora é a sua vez de personalizar a contagem !')
+ini = int(input('Inicio: '))
+final = int(input('Final: '))
+pas = int(input('Passo: '))
+
+contador(ini, final, pas)
