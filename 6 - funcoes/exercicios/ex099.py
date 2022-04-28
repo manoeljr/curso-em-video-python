84cf8efb7c9d9401615fbcf856b06ddce69e4e14
manoeljr <manoelv8@gmail.com>
2022-04-27 22:00:46 -0300

    Adicionando exercicios de FUNÇÃO

--- 6 - funcoes/exercicios/ex099.py
+++ 6 - funcoes/exercicios/ex099.py
@@ -4,3 +4,13 @@
     Seu programa tem que analisar todos os valores e
     dizer qual deles é o maior.
 """
+
+
+def maior(* valores):
+    if valores:
+        print(f'Maior valor {max(valores)}')
+    else:
+        print('Não existe valor maior')
+
+
+maior(1, 56, 99)
