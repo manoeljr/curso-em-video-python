36e657c2d6938133889da661eb53a5ffcfda0164
manoeljr <manoelv8@gmail.com>
2022-04-28 20:44:40 -0300

    Adicionando exercicios de FUNÇÃO

---
+++ 6 - funcoes/exercicios/ex102.py
@@ -0,0 +1,7 @@
+"""
+    Crie um programa que tenha uma função fatorial() que receba
+    dois parâmetro: O primeiro que indique o número a calcular
+    e o outro chamado show, que será um valor lógico(opcional)
+    indicando se será mostrado ou não na tela o processo de
+    cálculo do fatorial.
+"""
---
+++ 6 - funcoes/exercicios/ex103.py
@@ -0,0 +1,6 @@
+"""
+    Faça um programa que tenha uma função chamada ficha(), que receba dois
+    parâmetros opcionais: O nome de um jogador e quantos gols ele marcou.
+    O programa deverá ser capaz de mostrar a ficha do jagador, mesmo que
+    algum dado não tenha sido informado corretamente.
+"""
