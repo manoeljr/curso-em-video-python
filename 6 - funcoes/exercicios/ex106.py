8d055cc96f511afd4c10c4c244c05c4df47f11f3
manoeljr <manoelv8@gmail.com>
2022-04-29 17:45:25 -0300

    Adicionando exercicios de FUNÇÃO

--- 6 - funcoes/exercicios/ex106.py
+++ 6 - funcoes/exercicios/ex106.py
@@ -5,21 +5,3 @@
       OBS.: use cores.
 """
 
-
-def voto(ano):
-    from datetime import date
-
-    atual = date.today().year
-    idade = atual - ano
-
-    if idade < 16:
-        return f'Com {idade} anos: NÃO VOTA.'
-    elif 16 <= idade < 18 or idade > 65:
-        return f'Com {idade} anos: VOTO OPCIONAL.'
-    else:
-        return f'Com {idade} anos: VOTO OBRIGATÓRIO.'
-
-
-nascimento = int(input('Em que ano você nasceu ? '))
-print(voto(nascimento))
-
