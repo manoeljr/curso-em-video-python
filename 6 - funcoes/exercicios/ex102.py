564faa186e6d814956beb2188a08b68a3dd02063
manoeljr <manoelv8@gmail.com>
2022-04-29 22:42:15 -0300

    Adicionando exercicios de FUNÇÃO

--- 6 - funcoes/exercicios/ex102.py
+++ 6 - funcoes/exercicios/ex102.py
@@ -5,3 +5,31 @@
     indicando se será mostrado ou não na tela o processo de
     cálculo do fatorial.
 """
+
+
+def fatorial(numero, show=False):
+    """
+    Calcula o fatorial de um número
+    :param numero: O nũmero a ser calculado
+    :param show opcional: Mostra ou não a conta
+    :return: o fatorial do número
+    """
+    fatorial = 1
+    for cont in range(numero, 0, -1):
+        if show:
+            print(cont, end='')
+            if cont > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+        fatorial *= cont
+    return fatorial
+
+
+numero = int(input('Digite o valor do fatorial: '))
+show = str(input('Deseja verifica o fatorial [S/N]: ')).upper()[0]
+if show in 'Ss':
+    print(fatorial(numero, show=True))
+else:
+    print(fatorial(numero, show=False))
+
