14f2f88552c57e8d243325918d0729e469651a67
manoeljr <manoelv8@gmail.com>
2022-04-30 11:16:36 -0300

    Adicionando exercicios de FUNÇÃO

--- 6 - funcoes/exercicios/ex105.py
+++ 6 - funcoes/exercicios/ex105.py
@@ -8,3 +8,31 @@
       - A situação(Opcional)
     Adicione também as docstrings da função
 """
+
+
+def notas(*notas, situacao=False):
+    """
+    Função de notas e media de varios alunos
+    :param notas: Notas dos alunos
+    :param situacao(opcional): Situação do alunos em relação a sua média
+    :return: Retorna um dicionário com notas e media do aluno
+    """
+    retorno = {}
+    retorno['total'] = len(notas)
+    retorno['maior'] = max(notas)
+    retorno['menor'] = min(notas)
+    retorno['media'] = sum(notas) / len(notas)
+
+    if situacao:
+        if retorno['media'] >= 7:
+            retorno['situacao'] = 'BOA'
+        elif retorno['media'] >= 5:
+            retorno['situacao'] = 'RAZOAVEL'
+        else:
+            retorno['situacao'] = 'RUIM'
+
+    return retorno
+
+
+resposta = notas(9, 10, 5.5, 2.5, 8.5, situacao=True)
+print(resposta)
