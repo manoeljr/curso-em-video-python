0d1c7f1c7bfe6814de7073ef5fca57b396fa1969
manoeljr <manoelv8@gmail.com>
2022-04-29 22:53:12 -0300

    Adicionando exercicios de FUNÇÃO

--- 6 - funcoes/exercicios/ex103.py
+++ 6 - funcoes/exercicios/ex103.py
@@ -4,3 +4,22 @@
     O programa deverá ser capaz de mostrar a ficha do jagador, mesmo que
     algum dado não tenha sido informado corretamente.
 """
+
+
+def ficha(jogador='<desconhecido>', gol=0):
+    print(f'O Jogador {jogador} fez {gol} gol(s) no campeonato.')
+
+
+numero = str(input('Nome do jogador: '))
+gol = str(input('Número de gols: '))
+
+if gol.isnumeric():
+    gol = int(gol)
+else:
+    gol = 0
+
+
+if numero.strip() == '':
+    ficha(gol=gol)
+else:
+    ficha(numero, gol)
